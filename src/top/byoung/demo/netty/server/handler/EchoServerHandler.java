fe28c55af2686d36a178479dfe2c2efb696750d7
yangweixin <yangweixin@codemao.cn>
2018-11-27 14:19:50 +0800

    first commit

---
+++ src/top/byoung/demo/netty/server/handler/EchoServerHandler.java
@@ -0,0 +1,18 @@
+package top.byoung.demo.netty.server.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
