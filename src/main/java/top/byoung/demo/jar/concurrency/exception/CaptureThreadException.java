da923c7660cd78afecdaced299a1f1970ee638d4
yangweixin <yangweixin@codemao.cn>
2019-03-14 17:28:13 +0800

    feature: catch thread error.

--- src/main/java/top/byoung/demo/jar/concurrency/exception/CaptureThreadException.java
+++ src/main/java/top/byoung/demo/jar/concurrency/exception/CaptureThreadException.java
@@ -6,25 +6,33 @@ public class CaptureThreadException {
 
     public static void main(String[] args) {
 
-        ThreadGroup group = Thread.currentThread().getThreadGroup();
-        System.out.println(group.getName());
-        System.out.println(group.getParent().getName());
-        System.out.println(group.getParent().getParent().getName());
-//
+//        ThreadGroup group = Thread.currentThread().getThreadGroup();
+//        System.out.println(group.getName());
+//        System.out.println(group.getParent().getName());
+//        System.out.println(group.getParent().getParent().getName());
+
 //        Thread.setDefaultUncaughtExceptionHandler(
 //            ((t, e) -> {
-//                System.out.println(t.getName());
+//                System.out.println(">>>>>>2 "+ t.getName());
 //                e.printStackTrace();
 //            })
 //        );
-//
-//        new Thread(() -> {
-//            try {
-//                TimeUnit.SECONDS.sleep(2);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            int i = 1/0;
-//        }).start();
+
+        new Thread(new ChildThread()).start();
+    }
+}
+
+class ChildThread implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+            System.out.println("thread");
+            int i = 1/0;
+        } catch (Exception e) {
+            System.out.println(">>>>>>1 "+ Thread.currentThread().getName());
+            e.printStackTrace();
+        }
     }
 }
